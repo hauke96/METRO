bc1987b33279a84f254dbb6ce9f580cc7a565762
hauke96 <mail@hauke-stieler.de>
2015-03-04 16:19:20 +0100

    Just a few variable changes

--- src/Game/CityTravelerSpot.java
+++ src/Game/CityTravelerSpot.java
@@ -22,98 +22,100 @@ public class CityTravelerSpot
 	}
 	/**
 	 * Checks if mouse is in ONLY this circle or greater circles (but not in smaller/next ones).
-	 * @param circleIndex The circle index.
+	 * @param layerIndex The circle index.
 	 * @return Boolean if mouse is in circle.
 	 */
-	public boolean isMouseInCircle(int circleIndex)
+	public boolean isMouseInCircle(int layerIndex)
 	{
-		circleIndex = _strength - circleIndex;
-		if(circleIndex < 0) return false;
+		layerIndex = (int)_strength - layerIndex;
+		if(layerIndex < 0) return false;
 		
 		Point position = new Point(_position.x + (int)GameScreen_CityView._offset.getX(), _position.y + (int)GameScreen_CityView._offset.getY());
 
 		boolean isInCurrentCircle = Math.pow(MouseInfo.getPointerInfo().getLocation().x - position.x, 2) 
 				+ Math.pow(MouseInfo.getPointerInfo().getLocation().y - position.y, 2) 
-				< Math.pow(_circleRadiusStep * circleIndex, 2); // true: Mouse cursor is in circle
+				< Math.pow(_circleRadiusStep * layerIndex, 2); // true: Mouse cursor is in circle
 		boolean isInNextCircle = Math.pow(MouseInfo.getPointerInfo().getLocation().x - position.x, 2) 
 				+ Math.pow(MouseInfo.getPointerInfo().getLocation().y - position.y, 2) 
-				< Math.pow(_circleRadiusStep * (circleIndex - 1), 2); // true: Mouse cursor is in circle
+				< Math.pow(_circleRadiusStep * (layerIndex - 1), 2); // true: Mouse cursor is in circle
 		
 		return isInCurrentCircle && !isInNextCircle;
 	}
 	/**
 	 * Draws the circles of the Hotspot.
 	 * @param g The graphic handle to draw an.
-	 * @param circleIndex Index of the circle to draw
+	 * @param layerIndex Index of the circle to draw
 	 * @param circleSelected If the circle is selected and therefore drawn in a different color.
 	 * @param drawNumbers if numbers should be drawn on circle
 	 */
-	public void draw(Graphics2D g, int circleIndex, boolean circleSelected, boolean drawNumbers)
+	public void draw(Graphics2D g, int layerIndex, boolean circleSelected, boolean drawNumbers)
 	{
-		draw(g, circleIndex, circleSelected, drawNumbers, false);
+		draw(g, layerIndex, circleSelected, drawNumbers, false);
 	}
 	/**
 	 * Draws the circles of the Hotspot.
 	 * @param g The graphic handle to draw an.
-	 * @param circleIndex Index of the circle to draw
+	 * @param layerIndex Index of the circle to draw
 	 * @param circleSelected If the circle is selected and therefore drawn in a different color.
 	 * @param drawNumbers if numbers should be drawn on circle
 	 * @param onlyEdges If only edges should be drawn
 	 */
-	public void draw(Graphics2D g, int circleIndex, boolean circleSelected, boolean drawNumbers, boolean onlyEdges)
+	public void draw(Graphics2D g, int layerIndex, boolean circleSelected, boolean drawNumbers, boolean onlyEdges)
 	{
-		circleIndex = _strength - circleIndex;
-		if(circleIndex < 0) return;
+		layerIndex = _strength - layerIndex;
+		if(layerIndex < -1) return;
 		
 		// get the position with offset
 		Point position = new Point(_position.x + (int)GameScreen_CityView._offset.getX(), _position.y + (int)GameScreen_CityView._offset.getY());
 
 		if(circleSelected)
 		{
-			g.setColor(new Color(230 - 5 * (_strength - circleIndex), 240 - (int)(4f * (float)(_strength - circleIndex)), 255));
-			g.fillOval(position.x - circleIndex *_circleRadiusStep + 1, 
-					position.y - circleIndex * _circleRadiusStep + 1,
-					_circleRadiusStep * 2 * circleIndex - 1, 
-					_circleRadiusStep * 2 * circleIndex - 1);
+			g.setColor(new Color(230 - 5 * (int)(_strength - layerIndex), 
+					240 - (int)(3.5f * (_strength - layerIndex)), 
+					255));
+			g.fillOval(position.x - layerIndex *_circleRadiusStep + 1, 
+				position.y - layerIndex * _circleRadiusStep + 1,
+				_circleRadiusStep * 2 * layerIndex - 1, 
+				_circleRadiusStep * 2 * layerIndex - 1);
 
 			g.setColor(METRO.__metroBlue);
 			g.setColor(new Color(70, 126, 179));
-			if(drawNumbers && circleIndex > 1)
+			if(drawNumbers && layerIndex > 2)
 			{
-				g.drawString((_strength - circleIndex) + "", position.x + _circleRadiusStep * circleIndex - _circleRadiusStep / 2 - 5, position.y + 4);
-				g.drawString((_strength - circleIndex) + "", position.x - _circleRadiusStep * circleIndex + _circleRadiusStep / 2 - 1, position.y + 4);
+				g.drawString(_strength - layerIndex + "", position.x + _circleRadiusStep * layerIndex - _circleRadiusStep / 2 - 5, position.y + 4);
+				g.drawString(_strength - layerIndex + "", position.x - _circleRadiusStep * layerIndex + _circleRadiusStep / 2 - 1, position.y + 4);
 				
-				g.drawString((_strength - circleIndex) + "", position.x - 3, position.y + _circleRadiusStep * circleIndex - _circleRadiusStep / 2 + 5);
-				g.drawString((_strength - circleIndex) + "", position.x - 3, position.y - _circleRadiusStep * circleIndex + _circleRadiusStep - 15);
+				g.drawString(_strength - layerIndex + "", position.x - 3, position.y + _circleRadiusStep * layerIndex - _circleRadiusStep / 2 + 5);
+				g.drawString(_strength - layerIndex + "", position.x - 3, position.y - _circleRadiusStep * layerIndex + _circleRadiusStep - 15);
 			}
-			else if (circleIndex == 1 && drawNumbers) 
+			else if (layerIndex <= 2 && drawNumbers) 
 			{
 				g.drawString((_strength - 1) + "", position.x, position.y + 4);
 			}
 		}
 		else
 		{
-			int gray = 255 - 25 * (_strength - circleIndex + 1); // gray color based on layer
+			int gray = 255 - 25 * (_strength - layerIndex + 1); // gray color based on layer
 			if(onlyEdges)
 			{
 				g.setColor(new Color(gray, gray, gray));
-				g.drawOval(position.x - circleIndex *_circleRadiusStep, 
-						position.y - circleIndex * _circleRadiusStep,
-						_circleRadiusStep * 2 * circleIndex, 
-						_circleRadiusStep * 2 * circleIndex);
+				g.drawOval(position.x - layerIndex *_circleRadiusStep,
+					position.y - layerIndex * _circleRadiusStep,
+					_circleRadiusStep * 2 * layerIndex,
+					_circleRadiusStep * 2 * layerIndex);
 			}
 			else
 			{
-				gray = 255 - 5 * (_strength - circleIndex + 1); // gray color based on layer
+				gray = 255 - 5 * (_strength - layerIndex + 1); // gray color based on layer
 				g.setColor(new Color(gray, gray, gray));
-				g.fillOval(position.x - circleIndex *_circleRadiusStep + 1, 
-						position.y - circleIndex * _circleRadiusStep + 1,
-						_circleRadiusStep * 2 * circleIndex - 1, 
-						_circleRadiusStep * 2 * circleIndex - 1);
+				g.fillOval(position.x - layerIndex *_circleRadiusStep + 1,
+					position.y - layerIndex * _circleRadiusStep + 1,
+					_circleRadiusStep * 2 * layerIndex - 1,
+					_circleRadiusStep * 2 * layerIndex - 1);
 			}
 		}
 	}
-	public int getStrength()
+	public float getStrength()
 	{
 		return _strength;
 	}
